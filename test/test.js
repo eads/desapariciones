d43987a157e075daa72bb8a203b2829d7533dd14
David Eads <davideads@gmail.com>
2019-05-28 18:58:47 -0500

    remove failing test

--- test/test.js
+++ test/test.js
@@ -4,8 +4,3 @@ it('Should be my first successful test', function() {
   let success = true;
   success.should.be.true;
 });
-
-it('Should be my first failing test', function() {
-  let success = false;
-  success.should.be.true;
-});
