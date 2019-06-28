a42d8d9e1a2d1a5907558f9a8ab152ccaa1573cc
David Eads <davideads@gmail.com>
2019-06-27 22:47:02 -0500

    start cleaning up start kit

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ path: '../.env' })
 
 module.exports = {
   siteMetadata: {
@@ -27,6 +27,14 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `DESAPARICIONES`,
+        fieldName: `desapariciones`,
+        url: process.env.HASURA_API_URL,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
