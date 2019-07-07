327f8200d82882af3742cca62908ca34daae88a3
David Eads <davideads@gmail.com>
2019-07-07 14:04:54 -0500

    try with recharts (w some victory experiments)
    nivo is annoying and victory slows mapbox way down

--- site/src/components/layout.js
+++ site/src/components/layout.js
@@ -24,7 +24,6 @@ const Layout = ({ children }) => {
     }
   `)
 
-  console.log(data)
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -4,60 +4,117 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import DeckGL from "@deck.gl/react"
-import {ScatterplotLayer} from "@deck.gl/layers"
-import {StaticMap} from "react-map-gl"
+import ReactSwipe from "react-swipe"
 
-const MAPBOX_TOKEN = "pk.eyJ1IjoiYWRvbmRldmFuIiwiYSI6ImNqbm0yc2x3aDA0c2QzcXVteWhjaW5vZTMifQ.9iTMcfENx9TOCQ94oXEevQ"
+import ReactMapGL from "react-map-gl"
+import "mapbox-gl/dist/mapbox-gl.css"
 
-const INITIAL_VIEW_STATE = {
-  longitude: -101.12,
-  latitude: 23.42,
-  zoom: 2.7,
-  maxZoom: 16,
-  pitch: 0,
-  bearing: 0
-};
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
-const IndexPage = (context) => {
-  const data = context.data.desapariciones.views_areas_geoestadisticas_municipales_centroids
+const MAPBOX_TOKEN = "pk.eyJ1IjoiZGF2aWRlYWRzIiwiYSI6ImNpZ3d0azN2YzBzY213N201eTZ3b2E0cDgifQ.ZCHD8ZAk32iAp9Ue3tPVVg"
 
-  const layer = new ScatterplotLayer({
-    id: "scatterplot-layer",
-    data,
-    opacity: 1,
-    stroked: false,
-    filled: true,
-    radiusScale: 10,
-    radiusMinPixels: 1.25,
-    radiusMaxPixels: 80,
-    lineWidthMinPixels: 1,
-    getPosition: d => [d.lng, d.lat],
-    getRadius: d => d.cenapi_by_year_aggregate.aggregate.sum.count,
-    getFillColor: d => [255, 140, 0],
-    getLineColor: d => [0, 0, 0],
-  })
+class IndexPage extends React.Component {
+  state = {
+    viewport: {
+      width: '100%',
+      height: '100%',
+      longitude: -102.9,
+      latitude: 23.42,
+      zoom: 3.1,
+    }
+  }
 
-  const mapStyle = "mapbox://styles/mapbox/light-v9";
+  render() {
 
-  return( <Layout>
-    <SEO title="Map" />
+    let reactSwipeEl
+    const { processed_areas_geoestadisticas_estatales } = this.props.data.desapariciones
+const data = [
+  {
+    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+  },
+  {
+    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+  },
+  {
+    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+  },
+  {
+    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+  },
+  {
+    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+  },
+  {
+    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+  },
+  {
+    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+  },
+];
 
-    <div className="explorer-pane">
-      <h1>Swipey explorer goes here</h1>
-    </div>
+    const colors = ['#cc0000', '#00cc00',]
 
-    <div className="map-pane">
-      <DeckGL layers={[layer]} initialViewState={INITIAL_VIEW_STATE} controller={true}>
-        <StaticMap
-          reuseMaps
-          mapStyle={mapStyle}
-          preventStyleDiffing={true}
-          mapboxApiAccessToken={MAPBOX_TOKEN}
-        />
-      </DeckGL>
-    </div>
-  </Layout>)
+    return (
+      <Layout>
+        <SEO title="Map" />
+        <div className="explorer-pane">
+          <ReactSwipe
+            className="carousel"
+            swipeOptions={{ continuous: false }}
+            ref={el => (reactSwipeEl = el)}
+          >
+            <div className="item">
+              <div className="item-inner">
+                <h1>Disappearances</h1>
+                <h2>Subhed</h2>
+                <p>Graf about what's next</p>
+                <button onClick={() => reactSwipeEl.next()}>Explore the map</button>
+              </div>
+            </div>
+            <div className="item">
+              <div className="item-inner">
+                <LineChart
+                  width={300}
+                  height={200}
+                  data={processed_areas_geoestadisticas_estatales}
+                  margin={{
+                    top: 10, right: 10, left: 30, bottom: 10,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  {processed_areas_geoestadisticas_estatales.map(
+                    (d, i) => {
+                      return(<Line
+                        type="monotone"
+                        dataKey={(row) => {
+                          return (row.cve_ent == d.cve_ent) ? Math.random() : 0 
+                        }}
+                        stroke={i % 2 ? colors[0] : colors[1]}
+                        key={"line"+i}
+                      />)
+                    }
+                  )}
+                </LineChart>
+                <button onClick={() => reactSwipeEl.prev()}>Read the preview</button>
+              </div>
+            </div>
+          </ReactSwipe>
+
+        </div>
+
+        <div className="map-pane">
+          <ReactMapGL
+            {...this.state.viewport}
+            onViewportChange={(viewport) => this.setState({viewport})}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+            mapStyle="mapbox://styles/davideads/cjxhxnw2a3vz81cr178aj3syc"
+          />
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default IndexPage
@@ -65,23 +122,46 @@ export default IndexPage
 export const query = graphql`
   query {
     desapariciones {
-      views_areas_geoestadisticas_municipales_centroids(where: {cenapi_by_year: {cve_geoid: {_is_null: false}}}) {
-        nom_mun
+      processed_areas_geoestadisticas_estatales {
         nom_ent
-        lat
-        lng
-        cenapi_by_year_aggregate {
-          aggregate {
-            sum {
-              count
-            }
-          }
-        }
+        cve_ent
         cenapi_by_year {
           year
           count
+          cumulative_count
         }
       }
     }
   }
 `
+
+/*
+ *
+ *                 <VictoryChart
+                  domainPadding={10}
+                  height={200}
+                  width={400}
+                  theme={VictoryTheme.material}
+                  >
+                  <VictoryAxis dependentAxis
+                    tickFormat={(tick) => (tick / 1000) + 'k'}
+                  />
+                  <VictoryAxis
+                    tickFormat={(tick) => (tick)}
+                  />
+                  <VictoryLabel style={{size:5}}/>
+                  <VictoryStack>
+                    {processed_areas_geoestadisticas_estatales.map(
+                      (d, i) => (
+                        <VictoryArea
+                          interpolation="monotoneX"
+                          data={d.cenapi_by_year}
+                          x="year"
+                          y="cumulative_count"
+                          key={"Line-" + i}
+                        />
+                      )
+                    )}
+                  </VictoryStack>
+                  </VictoryChart>
+                  */
