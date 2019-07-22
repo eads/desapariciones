b6e8321294885351c9d414f002da0c954801162e
David Eads <davideads@gmail.com>
2019-07-21 22:18:02 -0500

    pull in viewport indicator component
    this import-at-top-level tactic may be a better approach than the current card stack

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -4,13 +4,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Cards from "../components/cards"
 import Map from "../components/map"
-
+import ViewportIndicator from "../charts/viewport-indicator"
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Map" />
       <div className="explorer-pane">
+        <ViewportIndicator />
         <Cards />
       </div>
       <div className="map-pane">
