c76bd515b39dacef47070eba0f553e8e0246e6e6
David Eads <davideads@gmail.com>
2021-03-15 20:33:28 -0500

    add gender/status combos

--- sql/views/cenapi_estado_by_month.sql
+++ sql/views/cenapi_estado_by_month.sql
@@ -13,7 +13,14 @@ SELECT
   count(*) filter (where c.sexo = 'MASCULINO') AS gender_masc_ct,
   count(*) filter (where c.vivo_o_muerto = 'VIVO') as status_alive_ct,
   count(*) filter (where c.vivo_o_muerto = 'AUN SIN LOCALIZAR') as status_not_found_ct,
-  count(*) filter (where c.vivo_o_muerto = 'MUERTO') as status_dead_ct
+  count(*) filter (where c.vivo_o_muerto = 'MUERTO') as status_dead_ct,
+  count(*) filter (where c.sexo = 'FEMENINO' and c.vivo_o_muerto = 'AUN SIN LOCALIZAR') as femenino_aun_sin_localizar,
+  count(*) filter (where c.sexo = 'FEMENINO' and c.vivo_o_muerto = 'VIVO') as femenino_vivo,
+  count(*) filter (where c.sexo = 'FEMENINO' and c.vivo_o_muerto = 'MUERTO') as femenino_muerto,
+  count(*) filter (where c.sexo = 'MASCULINO' and c.vivo_o_muerto = 'AUN SIN LOCALIZAR') as masculino_aun_sin_localizar,
+  count(*) filter (where c.sexo = 'MASCULINO' and c.vivo_o_muerto = 'VIVO') as masculino_vivo,
+  count(*) filter (where c.sexo = 'MASCULINO' and c.vivo_o_muerto = 'MUERTO') as masculino_muerto
+  
 FROM
   processed.cenapi c
 JOIN
